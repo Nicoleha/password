35841e39a862b5b43f2fafd733f359e7e8014cb8
Nicole Hategekimana <nicoleha127@gmail.com>
2019-02-11 12:17:17 +0200

    testing the function to save contact

--- user_test.py
+++ user_test.py
@@ -24,5 +24,14 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.create_pw,"term00")
         self.assertEqual(self.new_user.confirm_pw,"term00")
 
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() 
+        self.assertEqual(len(User.user_list),1)
+    
+
 if __name__ == '__main__':
     unittest.main()
