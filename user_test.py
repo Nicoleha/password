be84e267a3b5999dfc5679323b8019a3352e62c6
Nicole Hategekimana <nicoleha127@gmail.com>
2019-02-11 12:05:52 +0200

    testing user initiated

--- user_test.py
+++ user_test.py
@@ -2,10 +2,27 @@ import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
-     '''
+    '''
     Test class that defines test cases for the user class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-    
+    def setUp(self):
+        '''
+        set up method to run before each test
+        '''
+        self.new_user=User("nic","hat","term00","term00")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"nic")
+        self.assertEqual(self.new_user.last_name,"hat")
+        self.assertEqual(self.new_user.create_pw,"term00")
+        self.assertEqual(self.new_user.confirm_pw,"term00")
+
+if __name__ == '__main__':
+    unittest.main()
