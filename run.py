3a0839f28c764359f88efbb170170bf85b1b8afa
Nicole Hategekimana <nicoleha127@gmail.com>
2019-02-11 15:36:10 +0200

    program is now executable through run file in the terminal

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3.6
 from user import User
 from credential import Credential
+
+def create_credential(acc_name,password):
+    '''
+    function to create a new credential
+    '''
+    new_credential=Credential(acc_name,password)
+    return new_credential
