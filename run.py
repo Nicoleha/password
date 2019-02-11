8eef3b15524617eef1f059482b4a90e23eea92dc
Nicole Hategekimana <nicoleha127@gmail.com>
2019-02-11 15:43:23 +0200

    function that create user in run.py created

--- run.py
+++ run.py
@@ -2,9 +2,19 @@
 from user import User
 from credential import Credential
 
+def create_user(fname,lname,cpw,fpw):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(fname,lname,cpw,fpw)
+    return new_user
+
 def create_credential(acc_name,password):
     '''
     function to create a new credential
     '''
     new_credential=Credential(acc_name,password)
     return new_credential
+
+def save_users(user):
+       
